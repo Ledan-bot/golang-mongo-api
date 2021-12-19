7ec9f8d7d86da05a17892d4e1f78cb87a1b0a6b3
Nicholas Ledan <Nickledan5@gmail.com>
2021-12-18 21:32:45 -0500

    Completed Get Restaurants by Borough

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 var client *mongo.Client
 
-func GetRestaurants(w http.ResponseWriter, r *http.Request) {
+func GetAllRestaurants(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	var places []bson.M
 	coll := client.Database("sample_restaurants").Collection("restaurants")
@@ -53,7 +53,6 @@ func GetRestaurantByName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
 	params := mux.Vars(r)
 	name := params["name"]
-	fmt.Println(name)
 
 	coll := client.Database("sample_restaurants").Collection("restaurants")
 
@@ -66,6 +65,41 @@ func GetRestaurantByName(w http.ResponseWriter, r *http.Request) {
 		panic(s)
 	}
 	json.NewEncoder(w).Encode(result)
+}
+
+func GetRestaurantByBorough(w http.ResponseWriter, r *http.Request) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	borough := params["borough"]
+	var places []bson.M
+	coll := client.Database("sample_restaurants").Collection("restaurants")
+
+	cursor, err := coll.Find(context.TODO(), bson.D{{"borough", borough}})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var result bson.M
+		err := cursor.Decode((&result))
+		if err != nil {
+			fmt.Println("cursor.Next() error: ", err)
+			os.Exit(1)
+		} else {
+			places = append(places, result)
+		}
+	}
+
+	if err := cursor.Err(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message": "` + err.Error() + `"}`))
+		return
+	}
+
+	json.NewEncoder(w).Encode(places)
 }
 
 func main() {
@@ -81,7 +115,8 @@ func main() {
 	}
 	fmt.Println("HERE")
 	fmt.Println(databases)
-	router.HandleFunc("/restaurants", GetRestaurants).Methods("GET")
+	router.HandleFunc("/restaurants", GetAllRestaurants).Methods("GET")
+	router.HandleFunc("/restaurants/{borough}", GetRestaurantByBorough).Methods("GET")
 	router.HandleFunc("/restaurant/{name}", GetRestaurantByName).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8033", router))
 }
